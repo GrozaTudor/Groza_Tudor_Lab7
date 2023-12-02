fed8f134aa317fbfd0fa2cc054f9cb9a904450f3
Groza Tudor <tudorgroza1969@yahoo.com>
2023-12-02 22:45:46 +0200

    lab8

--- Groza_Tudor_Lab7/ListEntryPage.xaml.cs
+++ Groza_Tudor_Lab7/ListEntryPage.xaml.cs
@@ -1,3 +1,5 @@
+using Groza_Tudor_Lab7.Models;
+
 namespace Groza_Tudor_Lab7;
 
 public partial class ListEntryPage : ContentPage
@@ -6,4 +8,18 @@ public partial class ListEntryPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
 }
