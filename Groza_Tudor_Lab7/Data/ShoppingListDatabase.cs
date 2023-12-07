7bee0b614c1620bc666553e93d2f96078c28dc33
Groza Tudor <tudorgroza1969@yahoo.com>
2023-12-07 20:24:41 +0200

    lab9

--- Groza_Tudor_Lab7/Data/ShoppingListDatabase.cs
+++ Groza_Tudor_Lab7/Data/ShoppingListDatabase.cs
@@ -12,11 +12,16 @@ namespace Groza_Tudor_Lab7.Data
     public class ShoppingListDatabase
     {
         readonly SQLiteAsyncConnection _database;
+        private object product;
+
         public ShoppingListDatabase(string dbPath)
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
+
         public Task<List<ShopList>> GetShopListsAsync()
         {
             return _database.Table<ShopList>().ToListAsync();
@@ -38,9 +43,52 @@ namespace Groza_Tudor_Lab7.Data
                 return _database.InsertAsync(slist);
             }
         }
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+
         public Task<int> DeleteShopListAsync(ShopList slist)
         {
-            return _database.DeleteAsync(slist);
+            return _database.DeleteAsync(product);
+
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+            "select P.ID, P.Description from Product P"
+            + " inner join ListProduct LP"
+            + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+            shoplistid);
+        }
+
+        internal Task DeleteProductAsync(Product product)
+        {
+            throw new NotImplementedException();
         }
     }
 }
